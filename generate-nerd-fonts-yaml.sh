477cfc35803fdfe3a39fa70b7f207871b27505ce
Yue Yang <g1enyy0ung@gmail.com>
2019-06-20 18:17:13 +0800

    Update generate-nerd-fonts-yaml.sh

--- generate-nerd-fonts-yaml.sh
+++ generate-nerd-fonts-yaml.sh
@@ -1,5 +1,5 @@
-echo "Get nerd-fonts-generated.css ..."
+echo "Getting nerd-fonts-generated.css ..."
 
-curl -SL https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/css/nerd-fonts-generated.css -o resources/nerd-fonts-generated.css
+curl -sSL https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/css/nerd-fonts-generated.css -o resources/nerd-fonts-generated.css
 
-echo "Get it."
+echo "Over."
